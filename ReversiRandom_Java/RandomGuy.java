f4aa9580f8f4be2c66be6cc02694e738fff06d74
James Dansie <james@dansie.com>
2022-05-24 19:21:08 -0600

    pruned and other changes

--- ReversiRandom_Java/RandomGuy.java
+++ ReversiRandom_Java/RandomGuy.java
@@ -64,16 +64,13 @@ class RandomGuy {
     // validMoves is a list of valid locations that you could place your "stone" on this turn
     // Note that "state" is a global variable 2D list that shows the state of the game
     private int move() {
-        //IMPORTANT!! depth must be a multiple of 2
         int depthToExplore = 5;
-        int move = exploreState(state, depthToExplore, true);
-        System.out.println("Selected Move: " + move);
+        int move = exploreState(state, depthToExplore, 10000, true);
         return move;
     }
 
-    private int exploreState(int[][] givenState, int depthToExplore, boolean initialCall){
+    private int exploreState(int[][] givenState, int depthToExplore, int previousMax, boolean initialCall){
         int initialState[][] = copyState(givenState);
-        System.out.println("Exploring state, depth = " + depthToExplore);
         // base case
         if(depthToExplore == 0){
             return evaluateState(givenState);
@@ -83,17 +80,28 @@ class RandomGuy {
         int numValidMovesThisState = numValidMoves;
         int validMovesThisState[] = validMoves.clone();
         int moveValues[] = new int[numValidMovesThisState];
+        int maxSoFar = -10000;
         // for each possible move
         for( int i = 0; i < numValidMovesThisState; i++ ){
             // make copy of state
             int copyState[][] = copyState(givenState);
+
+            //prune
+            if(i > 0){
+                if(moveValues[i-1] > maxSoFar){
+                    maxSoFar = moveValues[i-1];
+                }
+            }
+            if(maxSoFar > previousMax){
+                return -1*maxSoFar;
+            }
 
             // make move
             givenState = makeMoveOnState(givenState, validMovesThisState[i]);
 
             // evaluate value of move using heuristic function
             switchPlayers();
-            moveValues[i] = exploreState(givenState, depthToExplore-1, false);
+            moveValues[i] = exploreState(givenState, depthToExplore-1, -1*maxSoFar, false);
             switchPlayers();
 
             // revert state to original
@@ -597,140 +605,6 @@ class RandomGuy {
             return buildsFromCornerUpLDag(givenState, i-1, j-1);
         }
     }
-//    private int evaluateState(int givenState[][]) {
-//        int stateValue = 0;
-//
-//        // add 1 for your square, subtract 1 for their square
-//        for(int i = 0; i < givenState.length; i++){
-//            for(int j = 0; j < givenState[i].length; j++){
-//                if(givenState[i][j] == me){
-//                    stateValue += 1;
-//                }else if(givenState[i][j] != 0){
-//                    stateValue -= 1;
-//                }
-//            }
-//        }
-//
-//        // add 3 for your square on edge not counting directly next to corner, subtract 3 for their square on edge
-//        for(int i = 1; i < givenState[0].length - 1; i++){
-//            if(givenState[0][i] == me) {
-//                stateValue += 3;
-//            } else if(givenState[0][i] != 0){
-//                stateValue -= 3;
-//            }
-//        }
-//        int maxIndex = givenState.length-1;
-//        for(int i = 1; i < givenState[maxIndex].length -1; i++){
-//            if(givenState[maxIndex][i] == me) {
-//                stateValue += 3;
-//            } else if(givenState[maxIndex][i] != 0){
-//                stateValue -= 3;
-//            }
-//        }
-//        for(int i = 1; i < givenState.length -1; i++){
-//            if(givenState[i][0] == me) {
-//                stateValue += 3;
-//            } else if(givenState[i][0] != 0){
-//                stateValue -= 3;
-//            }
-//        }
-//        for(int i = 1; i < givenState.length -1; i++){
-//            if(givenState[i][maxIndex] == me) {
-//                stateValue += 3;
-//            } else if(givenState[i][maxIndex] != 0){
-//                stateValue -= 3;
-//            }
-//        }
-//
-//        //want spots 2 away from corner
-//        if (givenState[0][2] == me) {
-//            stateValue += 3;
-//        } else if (givenState[0][2] != 0) {
-//            stateValue -= 5;
-//        }
-//        if (givenState[2][0] == me) {
-//            stateValue +=3;
-//        } else if (givenState[2][0] != 0) {
-//            stateValue -= 5;
-//        }
-//        if (givenState[maxIndex][2] == me) {
-//            stateValue += 3;
-//        } else if (givenState[maxIndex][2] != 0) {
-//            stateValue -= 5;
-//        }
-//        if (givenState[2][maxIndex] == me) {
-//            stateValue +=3;
-//        } else if (givenState[2][maxIndex] != 0) {
-//            stateValue -= 5;
-//        }
-//        if (givenState[0][maxIndex-2] == me) {
-//            stateValue += 3;
-//        } else if (givenState[0][maxIndex -2] != 0) {
-//            stateValue -= 5;
-//        }
-//        if (givenState[maxIndex-2][0] == me) {
-//            stateValue +=3;
-//        } else if (givenState[maxIndex-2][0] != 0) {
-//            stateValue -= 5;
-//        }
-//        if (givenState[maxIndex-2][maxIndex] == me) {
-//            stateValue +=3;
-//        } else if (givenState[maxIndex-2][maxIndex] != 0) {
-//            stateValue -= 5;
-//        }
-//        if (givenState[maxIndex][maxIndex-2] == me) {
-//            stateValue +=3;
-//        } else if (givenState[maxIndex][maxIndex-2] != 0) {
-//            stateValue -= 5;
-//        }
-//
-//        //don't want diagonal unless we control corner
-//        if (givenState[0][0] != 0 && givenState[1][1] == me) {
-//            stateValue -= 100;
-//        } else if (givenState[0][0] == me && givenState[1][1] == me) {
-//            stateValue += 10;
-//        }
-//        if (givenState[maxIndex][0] != 0 && givenState[maxIndex -1][1] == me) {
-//            stateValue -= 100;
-//        } else if (givenState[maxIndex][0] == me && givenState[maxIndex-1][1] == me) {
-//            stateValue += 10;
-//        }
-//        if (givenState[0][maxIndex] != 0 && givenState[1][maxIndex-1] == me) {
-//            stateValue -= 100;
-//        } else if (givenState[0][maxIndex] == me && givenState[1][maxIndex-1] == me) {
-//            stateValue += 10;
-//        }
-//        if (givenState[maxIndex][maxIndex] != 0 && givenState[maxIndex-1][maxIndex-1] == me) {
-//            stateValue -= 100;
-//        } else if (givenState[maxIndex][maxIndex] == me && givenState[maxIndex-1][maxIndex-1] == me) {
-//            stateValue += 10;
-//        }
-//
-//
-//        // add 15 for your square in corner, subtract 15 for their square in corner
-//        if(givenState[0][0] == me) {
-//            stateValue += 20;
-//        } else if(givenState[0][0] != 0){
-//            stateValue -= 25;
-//        }
-//        if(givenState[0][maxIndex] == me) {
-//            stateValue += 20;
-//        } else if(givenState[0][maxIndex] != 0){
-//            stateValue -= 25;
-//        }
-//        if(givenState[maxIndex][0] == me) {
-//            stateValue += 20;
-//        } else if(givenState[maxIndex][0] != 0){
-//            stateValue -= 25;
-//        }
-//        if(givenState[maxIndex][maxIndex] == me) {
-//            stateValue += 20;
-//        } else if(givenState[maxIndex][maxIndex] != 0){
-//            stateValue -= 25;
-//        }
-//
-//        return stateValue;
-//    }
 
     private int[][] copyState(int state[][]) {
         int newState[][] = new int[8][8];
@@ -745,7 +619,85 @@ class RandomGuy {
 
     private int[][] makeMoveOnState(int state[][], int move){
         state[move/8][move%8] = me;
+        changeColors(state, move/8, move%8, me);
         return state;
+    }
+
+    public static void changeColors(int givenState[][], int row, int col, int turn) {
+        int incx, incy;
+        
+        for (incx = -1; incx < 2; incx++) {
+            for (incy = -1; incy < 2; incy++) {
+                if ((incx == 0) && (incy == 0))
+                    continue;
+                checkDirectionExt(givenState, row, col, incx, incy, turn);
+            }
+        }
+    }
+
+    public static void checkDirectionExt(int state[][], int row, int col, int incx, int incy, int turn) {
+        int sequence[] = new int[7];
+        int seqLen;
+        int i, r, c;
+        
+        seqLen = 0;
+        for (i = 1; i < 8; i++) {
+            r = row+incy*i;
+            c = col+incx*i;
+        
+            if ((r < 0) || (r > 7) || (c < 0) || (c > 7))
+                break;
+        
+            sequence[seqLen] = state[r][c];
+            seqLen++;
+        }
+        
+        int count = 0;
+        for (i = 0; i < seqLen; i++) {
+            if (turn == 0) {
+                if (sequence[i] == 2)
+                    count ++;
+                else {
+                    if ((sequence[i] == 1) && (count > 0))
+                        count = 20;
+                    break;
+                }
+            }
+            else {
+                if (sequence[i] == 1)
+                    count ++;
+                else {
+                    if ((sequence[i] == 2) && (count > 0))
+                        count = 20;
+                    break;
+                }
+            }
+        }
+        
+        if (count > 10) {
+            if (turn == 0) {
+                i = 1;
+                r = row+incy*i;
+                c = col+incx*i;
+                while (state[r][c] == 2) {
+                    state[r][c] = 1;
+                    i++;
+                    r = row+incy*i;
+                    c = col+incx*i;
+                }
+            }
+            else {
+                i = 1;
+                r = row+incy*i;
+                c = col+incx*i;
+                while (state[r][c] == 1) {
+                    state[r][c] = 2;
+                    i++;
+                    r = row+incy*i;
+                    c = col+incx*i;
+                }
+            }
+        }
     }
 
     // generates the set of valid moves for the player; returns a list of valid moves (validMoves)
